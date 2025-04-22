c74b9acf2cdddeec93207a8d1315bb3c64bbcc3a
Jennifer Zheng <zjy258085@gmail.com>
2025-04-22 14:04:39 -0400

    add human to the new world

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,7 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Human human = new Human();
+        addObject(human, 100, 100);
     }
 }
