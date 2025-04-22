9d3fa4733a665f1221aa722b5fecfe7c1a8e3ba4
brave29 <147207172+brave29@users.noreply.github.com>
2025-04-22 14:40:05 -0400

    bee changes

--- MyWorld.java
+++ MyWorld.java
@@ -5,5 +5,8 @@ public class MyWorld extends World {
         super(600, 400, 1);
         Human human = new Human();
         addObject(human, 100, 100);
+        
+        Bee bee = new Bee();
+        addObject(bee, 100, 100);
     }
 }
